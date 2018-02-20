674d9ec2f114d5d79f08f7e6861abe93adff9807
Padang Perwira Yudha <terrificsubs@gmail.com>
2018-02-20 19:41:19 +0700

    Create wikipedia.py

---
+++ wikipedia.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("-k",
+                       "--kata",
+                       dest="kata",
+                       action="store",
+                       default=None,
+                        required=True,
+                        help="Kata yang ingin dicari")
+args = arg_parser.parse_args()
+
+tautan = 'https://id.wikipedia.org/wiki/' + args.kata
+halaman = urlopen(tautan)
+soup = BeautifulSoup(halaman, 'html.parser')
+thesaurus = soup.find( 'div', attrs={ 'class' : 'mw-parser-output' })
+for p in thesaurus:
+    p = thesaurus.find('p')
+    hasil = p.text.strip()
+print('')
+print(hasil)
+print('')
